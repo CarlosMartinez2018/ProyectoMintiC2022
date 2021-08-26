9499f94eacaa15fa25bd7b9a6f8bf38417aed16e
CarlosMartinez2018 <89560111+CarlosMartinez2018@users.noreply.github.com>
2021-08-25 21:58:22 -0500

    Versio 2
    Trabajando con archivo de Java

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+//# ProyectoMintiC2022
+//Proyecto del Modulo 3 de MisioTIC
+public class HolaMundo{
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
